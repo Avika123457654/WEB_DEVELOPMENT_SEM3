d6212e9af23d4d46995fa1e0c42b3e0f8e841e42
Avika <164730326+Avika123457654@users.noreply.github.com>
2026-08-13 11:52:35 +0530

    main App component with routing and pages

---
+++ ASSIGNMENT_03/QUESTION_1.jsx
@@ -0,0 +1,44 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+function Home() {
+  return <h1>Home Page</h1>;
+}
+
+function About() {
+  return <h1>About Page</h1>;
+}
+
+function Contact() {
+  return <h1>Contact Us Page</h1>;
+}
+
+function Services() {
+  return <h1>Services Page</h1>;
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/about">About</Link> |{" "}
+        <Link to="/contact">Contact Us</Link> |{" "}
+        <Link to="/services">Services</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/services" element={<Services />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
