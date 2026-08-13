586e411f97dd01880fc0efc5bc50ee357c8cb7d1
Avika <164730326+Avika123457654@users.noreply.github.com>
2026-08-13 11:17:02 +0530

    Update QUESTION_1.jsx

--- ASSIGNMENT_01/QUESTION_1.jsx
+++ ASSIGNMENT_01/QUESTION_1.jsx
@@ -1,1 +1,38 @@
+import { useState } from "react";
 
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const increase = () => {
+    if (count < 10) {
+      setCount(count + 1);
+    }
+  };
+
+  const decrease = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Counter</h1>
+
+      <h2>{count}</h2>
+
+      <button onClick={decrease} disabled={count === 0}>
+        Decrease
+      </button>
+
+      <button onClick={increase} disabled={count === 10}>
+        Increase
+      </button>
+
+      {count === 0 && <p>Minimum limit reached!</p>}
+      {count === 10 && <p>Maximum limit reached!</p>}
+    </div>
+  );
+}
+
+export default Counter;
